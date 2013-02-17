aea4fd9ce000fd9c21b3a83de9f9bccff9ad1327
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2013-02-17 01:45:24 +0100

    Added sample page.

--- bin/sample-data.sh
+++ bin/sample-data.sh
@@ -4,3 +4,4 @@ export FINK_BASE="http://localhost:8080"
 
 curl $FINK_BASE/admin/api/posts -H "Content-type: application/json" --data @data/post1.json
 curl $FINK_BASE/admin/api/posts -H "Content-type: application/json" --data @data/post2.json
+curl $FINK_BASE/admin/api/pages -H "Content-type: application/json" --data @data/page1.json
