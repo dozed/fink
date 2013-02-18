2bf1cd91a8878c0470046cb4412ab9d9b9b1eb9a
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2013-02-18 01:18:40 +0100

    Set method for settings sample data to PUT.

--- bin/sample-data.sh
+++ bin/sample-data.sh
@@ -5,4 +5,4 @@ export FINK_BASE="http://localhost:8080"
 curl $FINK_BASE/admin/api/posts -H "Content-type: application/json" --data @data/post1.json
 curl $FINK_BASE/admin/api/posts -H "Content-type: application/json" --data @data/post2.json
 curl $FINK_BASE/admin/api/pages -H "Content-type: application/json" --data @data/page1.json
-curl $FINK_BASE/admin/api/settings -H "Content-type: application/json" --data @data/settings.json
+curl $FINK_BASE/admin/api/settings -X PUT -H "Content-type: application/json" --data @data/settings.json
