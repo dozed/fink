c59fe7135957f95019d1c5ce1b0a1d6304e62743
Mark Mynsted <code@growingliberty.com>
2014-05-30 14:15:02 -0500

    Trying to get tests to compile and run

--- src/test/scala/fink/JsonTests.scala
+++ src/test/scala/fink/JsonTests.scala
@@ -6,32 +6,32 @@ import fink.data._
 
 import org.specs2.mutable._
 
-import net.liftweb.json._
-import net.liftweb.json.JsonDSL._
-import net.liftweb.json.Serialization.{read => jsread, write => jswrite}
+import org.json4s._
+import org.json4s.jackson._
+import org.json4s.jackson.Serialization.{read => jsread, write => jswrite}
 
-class JsonTests extends Specification {
+ class JsonTests extends Specification {
 
-	implicit val formats = Serialization.formats(ShortTypeHints(List(classOf[Page], classOf[Category], classOf[Post], classOf[Tag]))) + FieldSerializer[Post]()
+ 	implicit val formats = Serialization.formats(ShortTypeHints(List(classOf[Page], classOf[Category], classOf[Post], classOf[Tag]))) + FieldSerializer[Post]()
 
-	"should de/serialize posts" in {
-		val post = Post(1,2,3,"title","author","text")
-		post.tags = List(Tag(0, "foo"), Tag(1, "bar"))
-		post.category = Some(Category(0, "cat"))
+ 	"should de/serialize posts" in {
+ 		val post = Post(1,2,3,"title","author","shortlink", "text")
+ 		post.tags = List(Tag(0, "foo"), Tag(1, "bar"))
+ 		post.category = Some(Category(0, "cat"))
 
-		val json = jswrite[Post](post)
-		val post2 = jsread[Post](json)
+ 		val json = jswrite[Post](post)
+ 		val post2 = jsread[Post](json)
 
-		post2 must beEqualTo(post)
-		post2.tags must containAllOf(post.tags)
-		post2.category must beEqualTo(post.category)
-	}
+ 		post2 must beEqualTo(post)
+ 		post2.tags must containAllOf(post.tags)
+ 		post2.category must beEqualTo(post.category)
+ 	}
 
-	"should handle missing options" in {
-		val json = """{"tags":[],"id":1,"catId":0,"date":0,"title":"title","author":"author","text":"text"}"""
-		val post = jsread[Post](json)
-		post.category must beNone
-		post.tags must have size(0) 
-	}
+ 	"should handle missing options" in {
+ 		val json = """{"tags":[],"id":1,"catId":0,"date":0,"title":"title","author":"author","shortlink":"shortlink","text":"text"}"""
+ 		val post = jsread[Post](json)
+ 		post.category must beNone
+ 		post.tags must have size(0)
+ 	}
 
-}
+ }
